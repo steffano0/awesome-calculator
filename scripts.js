7c8ff1b996d6b0e5b2abc90dac419728b30e2939
steffano0 <steffanouromero@gmail.com>
2023-03-28 15:31:38 -0500

    Create a function that checks if the number length is less than 16

--- scripts.js
+++ scripts.js
@@ -5,7 +5,25 @@ const operations = {
     "/": (a,b) => a/b,
     "^": (a,b) => a ** b,
 };
+
+function checkNumberLength () {
+    const lengthNumber = textDisplayed.length;
+    if (lengthNumber < 16) {
+        textDisplayed += this.textContent;
+    }
+}
     
+
+
+
+let textDisplayed = "";
+const numbers = Array.from(document.querySelectorAll(".number"));
+numbers.forEach(number => number.addEventListener("click", checkNumberLength));
+
+
+
+
+
 
 
 
